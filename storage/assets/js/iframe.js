42de8c6058e747e7741ed05db49a7363e7c64549
petezahv4 <petezahiscool@gmail.com>
2025-02-09 10:43:07 -0500

    Update iframe.js

--- storage/assets/js/iframe.js
+++ storage/assets/js/iframe.js
@@ -3,8 +3,13 @@ const url = urlParams.get('url');
 const iframe = document.getElementById('myIframe');
 
 if (url) {
-  iframe.src = url;
-  localStorage.setItem('iframeUrl', url);
+  // Extract the part after embed.html
+  const urlParts = url.split('embed.html');
+  const extraPart = urlParts.length > 1 ? urlParts[1] : '';
+
+  // Construct the iframe source URL with the extra part
+  iframe.src = urlParts[0] + 'embed.html' + extraPart;
+  localStorage.setItem('iframeUrl', iframe.src);
   history.replaceState(null, '', window.location.pathname);
 } else {
   const savedUrl = localStorage.getItem('iframeUrl');
@@ -15,43 +20,4 @@ if (url) {
   }
 }
 
-function toggleFullscreen() {
-            const iframe = document.getElementById('myIframe');
-            if (iframe.requestFullscreen) {
-                iframe.requestFullscreen();
-            } else if (iframe.mozRequestFullScreen) { 
-                iframe.mozRequestFullScreen();
-            } else if (iframe.webkitRequestFullscreen) { 
-                iframe.webkitRequestFullscreen();
-            } else if (iframe.msRequestFullscreen) { 
-                iframe.msRequestFullscreen();
-            }
-        }
-
-        function refreshIframe() {
-            const iframe = document.getElementById('myIframe');
-            iframe.src = iframe.src; 
-        }
-
-document.getElementById('whysofeinious-button').addEventListener('click', function() {
-            const script = document.createElement('script');
-            script.src = 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3';
-            script.async = true;
-            script.defer = true;
-            document.body.appendChild(script);
-
-            script.onload = function() {
-                new Crate({
-                    server: '1272664240175448208', 
-                    channel: '1272664509156298773' 
-                });
-            };
-        });
-
-document.getElementById('openIframeSource').addEventListener('click', function() {
-    var iframeSrc = document.getElementById('myIframe').src;
-    var link = document.createElement('a');
-    link.href = iframeSrc;
-    link.target = '_blank';
-    link.click();
-});
+// ... rest of the code (fullscreen, refresh, chat, open source)
